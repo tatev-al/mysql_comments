1edb43d71eada7a699d551c4c370e644b8fda599
tatev-al <tatevik.aleksanyan19@gmail.com>
2021-03-27 08:58:20 +0400

    add files

--- comments.php
+++ comments.php
@@ -20,55 +20,88 @@ $result = mysqli_query($connection, "SELECT users.name,comments.comment,comments
 <head>
   <title>Comments</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">    
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" href="#">
+  <script type="text/javascript">
+    $(document).ready(function(){
+        function load_data(query)
+        {
+            var xhr = $.ajax({
+                url:"search.php",
+                method:"POST",
+                dataType: "json",
+                data:{query:query},
+                beforeSend:function() {
+                    if(xhr != null) {
+                        xhr.abort();
+                    }
+                },
+                success:function(response)
+                {
+                    $('#result').empty();
+                    
+                    if (response.error) 
+                    {
+                        $('#result').append(response.message);
+                    }
+                    else
+                    {
+                        var result = "<div class=\"card-columns\">";
+                        $.each(response["data"], function(key, value) {
+                            
+                            result += "<div class=\"card p-3\">" + 
+                            "<blockquote class=\"blockquote mb-0 card-body\">" +
+                            "<p>" + value.comment + "</p>" +
+                            "<footer class=\"blockquote-footer\">" +
+                            "<small class=\"text-muted\">" + value.name +
+                            "<cite title=\"created at\"> " + value.created_at +
+                            "</cite>" + "</small>" + "</footer>" +
+                            '<a href=\"edit.php?id=' + value.id + '"' + " class=\"stretched-link\">Edit</a>" +
+                            "</blockquote>"+ "</div>";
+                        });
+                        result += "</div>";
+                            $('#result').append(result);
+                    } 
+                    
+                },
+                error:function()
+                {
+                    $('#result').empty();
+                    $('#result').append('No result');
+                    return;
+                }
+            });
+        }
+        $('#search').keyup(function()
+        {
+            var search = $(this).val();
+            if(search.length > 2)
+            {
+                load_data(search);
+            }
+            else
+            {
+                $('#result').empty();
+                $('#result').append('Type more than 2 characters');
+            }
+        });
+    });
+</script>  
 </head>
 <body>
     <div class="card-columns">
         <input type="text" class="form-control" id="search" placeholder="Search">
     </div>
-    <div id="result"></div>
+    <div id="result">
+    <div class="card-columns">
     <?php 
-    if(isset($_SESSION['id']))
-    {
-        $session_id = $_SESSION['id'];
-        if(mysqli_query($connection, "SELECT `name` FROM users WHERE users.id = $session_id"))
-        {
-            while(($record = mysqli_fetch_assoc($result)))
-            {?>
-                <div class="card-columns">
-                    <div class="card p-3">
-                        <blockquote class="blockquote mb-0 card-body">
-                            <p><?= $record['comment'] ?></p>
-                            <footer class="blockquote-footer">
-                                <small class="text-muted">
-                                    <?= $record['name'] ?> <cite title="created aty"><?= $record['created_at'] ?></cite>
-                                </small>
-                            </footer>
-                            <?php 
-                            if(isset($_SESSION['id']) && $record['user_id'] == $_SESSION['id'])
-                            {?>
-                                <small>
-                                    <a href="edit.php?id= <?= $record['id'] ?>" class="stretched-link">Edit</a>
-                                </small>
-                            <?php
-                            }
-                            ?>
-                        </blockquote>
-                    </div>
-                </div>
-                <?php
-            }
-        }
-    }
-    else
-    {
+
         while(($record = mysqli_fetch_assoc($result)))
         {?>
-            <div class="card-columns">
                 <div class="card p-3">
                     <blockquote class="blockquote mb-0 card-body">
                         <p><?= $record['comment'] ?></p>
@@ -77,42 +110,23 @@ $result = mysqli_query($connection, "SELECT users.name,comments.comment,comments
                                 <?= $record['name'] ?> <cite title="created at"><?= $record['created_at'] ?></cite>
                             </small>
                         </footer>
+                        <?php 
+                        if(isset($_SESSION['id']) && $record['user_id'] == $_SESSION['id'])
+                        {?>
+                            <small>
+                                <a href="edit.php?id= <?= $record['id'] ?>" class="stretched-link">Edit</a>
+                            </small>
+                        <?php
+                        }
+                        ?>
                     </blockquote>
                 </div>
-            </div>
             <?php
         }
-    }
-    ?> 
-    <script type="text/javascript">
-    $(document).ready(function(){
-            load_data();
-            function load_data(query)
-            {
-                $.ajax({
-                    url:"search.php",
-                    method:"POST",
-                    data:{query:query},
-                    success:function(data)
-                    {
-                        $('#result').html(data);
-                    }
-                });
-            }
-            $('#search').keyup(function()
-            {
-                var search = $(this).val();
-                if(search != '')
-                {
-                    load_data(search);
-                }
-                else
-                {
-                    load_data();
-                }
-            });
-        });
-    </script>           
+    ?>
+    </div>
+    </div>
+         
 </body>
 </html>
 
--- search.php
+++ search.php
@@ -2,31 +2,23 @@
     if(isset($_POST["query"]))
     {
         require "db_config.php";
+        session_start();
         $connection = mysqli_connect($servername, $username, $password, $db_name);
         $text = $_POST['query'];
-        $sql = "SELECT comments.*, users.name FROM comments, users WHERE user_id = users.id AND comment LIKE '%$text%'";
+        sleep(2);
+        $sql = "SELECT comments.*, users.name FROM comments INNER JOIN users ON user_id = users.id AND comment LIKE '%$text%'";
         $result = mysqli_query($connection, $sql);
-        //var_dump(mysqli_fetch_assoc($result));exit();
-        echo '<div class="table-responsive">
-        <table class="table table bordered">
-         <tr>
-          <th>Comment</th>
-          <th>Name</th>
-          <th>Created at</th>
-         </tr>';
-        if(mysqli_num_rows($result) > 0 && strlen($text) > 2)
+        $array = [];
+        if(mysqli_num_rows($result) > 0)
         {
-            while($row = mysqli_fetch_assoc($result))
+            while($record = mysqli_fetch_assoc($result))
             {
-                echo "	<tr>
-                            <td>".$row['comment']."</td>
-                            <td>".$row['name']."</td>
-                            <td>".$row['created_at']."</td>
-                        </tr>";
+                $array[] = $record;
             }
+            echo json_encode(['error' => false, 'data' => $array]);
         }
         else
         {
-            echo "<tr><td>0 result's found</td></tr>";
+            echo json_encode(['error' => true, 'message' => 'No result']);
         }
     }
